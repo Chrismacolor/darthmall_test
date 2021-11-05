b410d13d2fa60f8fd000bfc08df82eb74ab606c5
Chrismacolor <disanto@gmail.com>
2021-11-05 00:09:06 -0700

    forgot to import

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <Layout>
-      Darth Malls NFT
+     <h1>Darth Malls NFT</h1> 
     </Layout>
   )
 }
